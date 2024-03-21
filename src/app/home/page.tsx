28bf4ee4dc6f046bbdf5d28feb77fa6e9593e141
florian-leblanc <leblanc.florian@hotmail.fr>
2024-03-21 22:59:18 +0100

    IHM ultime

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -3,7 +3,7 @@ import styles from "@/app/page.module.css";
 export default function Home() {
     return (
         <>
-                <h1>Home</h1>
+                <h1 className="text-3xl font-bold underline text-red-500">Home</h1>
         </>
     );
 }
